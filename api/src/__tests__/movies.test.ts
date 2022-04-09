3a288b93b7636af8eaf3ee2288d2fb3236880dbf
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-09 21:07:21 +0530

    :test_tube: Adding a tests for movies routes

--- api/src/__tests__/movies.test.ts
+++ api/src/__tests__/movies.test.ts
@@ -22,3 +22,68 @@ describe("GET /movies", () => {
     expect(res.body.data).toBeDefined();
   });
 });
+
+describe("GET /genres", () => {
+  it("return a list of genres", async () => {
+    const res = await request(app).get("/genres");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
+
+describe("GET /movies/1", () => {
+  it("return movie details of specific movie", async () => {
+    const res = await request(app).get("/movies/1");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
+
+describe("GET /genres/12", () => {
+  it("return genre details of specific genre", async () => {
+    const res = await request(app).get("/genres/12");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
+
+describe("GET /movies/sort/popularity", () => {
+  it("return a list of movies sorted by popularity", async () => {
+    const res = await request(app).get("/movies/sort/popularity");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
+
+describe("GET /movies/filter/Action", () => {
+  it("return a list of movies filtered by genre", async () => {
+    const res = await request(app).get("/movies/filter/Action");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
