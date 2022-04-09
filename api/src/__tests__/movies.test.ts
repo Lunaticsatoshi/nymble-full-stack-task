f5bbb29eedb5b3a54ceb10882a4a85091c53f841
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-09 20:55:57 +0530

    :test_tube: Adding a test for get movies route

--- api/src/__tests__/movies.test.ts
+++ api/src/__tests__/movies.test.ts
@@ -1,3 +1,6 @@
+import app from "../index";
+import request from "supertest";
+
 // describe('list movies', () => {
 //     test('List all movies', async () => {
 //         expect(movies).toBe([]);
@@ -6,3 +9,16 @@
 //         expect(movies).toBe([]);
 //     })
 // })
+
+describe("GET /movies", () => {
+  it("return a list of movies", async () => {
+    const res = await request(app).get("/movies");
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toBeDefined();
+  });
+});
