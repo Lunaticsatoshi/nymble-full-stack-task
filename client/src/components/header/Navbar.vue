dfad8ff6f7340d7b57cbb2a5538ea9cf161aa93e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-04-09 21:29:39 +0530

    :sparkles: Created navbar component

---
+++ client/src/components/header/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar class="nav">
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        JMovies
+      </b-navbar-item>
+    </template>
+    <template #start>
+      <router-link to="/">
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+      </router-link>
+      <router-link to="/about">
+        <b-navbar-item href="#">
+          About
+        </b-navbar-item>
+      </router-link>
+    </template>
+  </b-navbar>
+</template>
+
+<style scoped lang="scss">
+.nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
